bee9ee7f080a8b353e7cfb8e97629a12d0bef05e
jadeqnguyen <jqnguyen@miners.utep.edu>
2024-05-08 00:09:20 -0600

    final

--- final project.py
+++ final project.py
@@ -6,24 +6,22 @@ dataset_1 = pd.read_csv('vgsales.csv')
 dataset_2 = pd.read_csv('vgsales-12-4-2019-short.csv')
 dataset_3 = pd.read_csv('vgsales-12-4-2019.csv')
 
-print(dataset_1.columns)
-# (['Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher', 'NA_Sales',
-#        'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales'],
-#       dtype='object')
-print(dataset_2.columns)
-# (['Rank', 'Name', 'Genre', 'ESRB_Rating', 'Platform', 'Publisher',
-#        'Developer', 'Critic_Score', 'User_Score', 'Total_Shipped',
-#        'Global_Sales', 'NA_Sales', 'PAL_Sales', 'JP_Sales', 'Other_Sales',
-#        'Year'],
-#       dtype='object')
-print(dataset_3.columns)
-# (['Rank', 'Name', 'basename', 'Genre', 'ESRB_Rating', 'Platform',
-#        'Publisher', 'Developer', 'VGChartz_Score', 'Critic_Score',
-#        'User_Score', 'Total_Shipped', 'Global_Sales', 'NA_Sales', 'PAL_Sales',
-#        'JP_Sales', 'Other_Sales', 'Year', 'Last_Update', 'url', 'status',
-#        'Vgchartzscore', 'img_url'],
-#       dtype='object')
+# print(dataset_1.columns)
+# # (['Rank', 'Name', 'Platform', 'Year', 'Genre', 'Publisher', 'NA_Sales',
+# #        'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales'],
+# #       dtype='object')
+# print(dataset_2.columns)
+# # (['Rank', 'Name', 'Genre', 'ESRB_Rating', 'Platform', 'Publisher',
+# #        'Developer', 'Critic_Score', 'User_Score', 'Total_Shipped',
+# #        'Global_Sales', 'NA_Sales', 'PAL_Sales', 'JP_Sales', 'Other_Sales',
+# #        'Year'],
+# #       dtype='object')
+# print(dataset_3.columns)
+# # (['Rank', 'Name', 'basename', 'Genre', 'ESRB_Rating', 'Platform',
+# #        'Publisher', 'Developer', 'VGChartz_Score', 'Critic_Score',
+# #        'User_Score', 'Total_Shipped', 'Global_Sales', 'NA_Sales', 'PAL_Sales',
+# #        'JP_Sales', 'Other_Sales', 'Year', 'Last_Update', 'url', 'status',
+# #        'Vgchartzscore', 'img_url'],
+# #       dtype='object')
 
-print(dataset_1.describe())
 print(dataset_2.describe())
-print(dataset_3.describe())
